5dc084ccfa7ad016424011412aa4c02dc922e0b2
mosuzi <jingjie.song@definesys.com>
2024-04-19 00:13:09 +0800

    修复：兼容unocss的grid格式

--- layouts/gridbase.vue
+++ layouts/gridbase.vue
@@ -16,9 +16,9 @@ export default defineComponent({
         case '1-1':
           return 'grid-cols-2'
         case '1-2':
-          return 'grid-cols-[1fr,2fr]'
+          return 'grid-cols-[1fr_2fr]'
         case '2-1':
-          return 'grid-cols-[2fr,1fr]'
+          return 'grid-cols-[2fr_1fr]'
         default:
           return props.cols
       }
@@ -34,7 +34,7 @@ export default defineComponent({
 <template>
   <div
     class="slidev-layout grid gap-y-3 gap-x-5 default"
-    :class="[!hideTitleRow ? 'grid-rows-[60px,1fr]' : null, gridColsSize]"
+    :class="[!hideTitleRow ? 'grid-rows-[60px_1fr]' : null, gridColsSize]"
   >
     <div v-if="!hideTitleRow" :class="cols ? 'col-span-2' : null">
       <h1>{{ $frontmatter.title }}</h1>
